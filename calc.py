e46a2de5082457c7494357d892ac3c3d19e7b982
Piotr Dickert <pmdickert@gmail.com>
2020-06-12 13:07:04 +0200

    Made change to a calc.py

---
+++ calc.py
@@ -0,0 +1,16 @@
+def add(x, y):
+    pass
+
+
+def subtract(x, y):
+    pass
+
+
+def multiply(x, y):
+    pass
+
+
+def divide(x, y):
+    if y == 0:
+        raise ValueError('Cannot divide by zero!')
+    return x // y
